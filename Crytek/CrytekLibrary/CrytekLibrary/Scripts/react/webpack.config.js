fe0dfa5fd7f14775f9ea7ef1c6f40bb025fcc38d
cornel1923 <cornel.gheorghita.cg@gmail.com>
2019-02-26 00:43:07 +0200

    add app

---
+++ Crytek/CrytekLibrary/CrytekLibrary/Scripts/react/webpack.config.js
@@ -0,0 +1,25 @@
+﻿
+module.exports = {
+    mode: 'production',
+    context: __dirname,
+    entry: "./app.js",
+    output: {
+        path: __dirname + '/build',
+        filename: "main.js"
+    },
+    watch: true,
+    module: {
+        // configuration regarding modules
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env', 'babel-preset-react']
+                    }
+                }
+            }]
+    }
+}
---
+++ Crytek/CrytekLibrary/CrytekLibrary/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrytekLibrary
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Default}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
